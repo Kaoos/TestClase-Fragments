fac834f0e447ead7de4cb02629002796e8bf836a
Rubén Ruiz <rubenruizherrera@gmail.com>
2015-03-03 09:24:50 +0100

    cambia el fragment
    al pulsar el botón de setings cambia el fragment

--- app/src/main/java/com/example/ruben/pr02/Frag4.java
+++ app/src/main/java/com/example/ruben/pr02/Frag4.java
@@ -3,7 +3,6 @@ package com.example.ruben.pr02;
 
 import android.os.Bundle;
 import android.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Frag1 extends Fragment {
+public class Frag4 extends Fragment {
 
 
-    public Frag1() {
+    public Frag4() {
         // Required empty public constructor
     }
 
@@ -24,7 +23,7 @@ public class Frag1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag1, container, false);
+        return inflater.inflate(R.layout.fragment_frag4, container, false);
     }
 
 
--- app/src/main/java/com/example/ruben/pr02/MainActivity.java
+++ app/src/main/java/com/example/ruben/pr02/MainActivity.java
@@ -15,22 +15,15 @@ public class MainActivity extends ActionBarActivity {
 
         ///miramos si es la primera vez que se crea la pantalla, si es la primera vez realiza el if sino pasa de el
 
-        if (savedInstanceState !=null)
-
-
-            return;
+        if (savedInstanceState !=null) {
+          return;
+        }
         // esto permite manipular los fragments
 
         getFragmentManager()
         .beginTransaction()
-        .add(R.id.container01, new Frag1())
+        .add(R.id.container01, new Frag4())
         .commit();
-
-
-
-
-
-
 
     }
 
@@ -49,11 +42,23 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container01, new Frag3())
+                    .commit();
             return true;
         }
 
+
+
+
+
+
         return super.onOptionsItemSelected(item);
+
     }
 }
